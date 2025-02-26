aba32984cef45176a404303735c014d3936ba20e
syedamerjunaid <saamerjunaid@gmail.com>
2025-02-26 17:07:22 +0530

    Library Section

--- script.js
+++ script.js
@@ -73,3 +73,17 @@ document.addEventListener("DOMContentLoaded", function () {
     typeText();
 });
 
+function searchBooks() {
+    let input = document.getElementById("searchBar").value.toLowerCase();
+    let books = document.querySelectorAll(".book");
+
+    books.forEach(book => {
+        let title = book.getAttribute("data-title").toLowerCase();
+        if (title.includes(input)) {
+            book.style.display = "block";
+        } else {
+            book.style.display = "none";
+        }
+    });
+}
+
